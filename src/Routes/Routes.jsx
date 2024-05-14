1f3930fe88dde0696a45c4bace3ed6e6543bae31
ArifMiah07 <arifmiah.me322@gmail.com>
2024-05-14 06:52:18 +0600

    git and github setups and created primary folder overview

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,40 @@
+import {
+    createBrowserRouter,
+  } from "react-router-dom";
+import Main from "../Layouts/Main/Main";
+import Home from "../Pages/Home/Home/Home";
+
+
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Main></Main>,
+    //   errorElement: <ErrorPage></ErrorPage>,
+      children: [
+        {
+            path: '/',
+            element: <Home></Home>
+        },
+        // {
+        //     path: '/login',
+        //     element: <LogIn></LogIn>
+        // },
+        // {
+        //     path: '/sign-up',
+        //     element: <SignUp></SignUp>
+        // },
+        // {
+        //     path: '/checkOut/:id',
+        //     element: <PrivateRoute> <CheckOut></CheckOut></PrivateRoute>,
+        //     loader: ({params})=> fetch(`https://car-doctor-sever-main.vercel.app/services/${params.id}`)
+        // },
+        // {
+        //     path: '/bookings',
+        //     element: <PrivateRoute><Bookings></Bookings></PrivateRoute>
+        // }
+      ]
+    },
+  ]);
+
+
+  export default router;
